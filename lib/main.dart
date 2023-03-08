cfd1df39cda586b7f89f5f496139bbde29b78097
Sarath Kumar M V <sarathkumarmv1@gmail.com>
2023-03-08 16:08:06 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_auth/provider/auth_provider.dart';
+import 'package:phone_auth/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ],
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: WelcomeScreen(),
+          title: "FlutterPhoneAuth"
+        ),
+    );
+
+  }
+  
+}
